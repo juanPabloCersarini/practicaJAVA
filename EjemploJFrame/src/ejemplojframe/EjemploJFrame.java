23abe56df60964ba6899ab961137bb635162bbf5
juanPabloCesarini <jpablocesarini@gmail.com>
2019-09-08 19:09:40 -0300

    primer trabajo con JFRAME - 08-09-2019

---
+++ EjemploJFrame/src/ejemplojframe/EjemploJFrame.java
@@ -0,0 +1,18 @@
+
+package ejemplojframe;
+
+/**
+ *
+ * @author Juan Pablo
+ */
+public class EjemploJFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana miVentana = new Ventana();
+        miVentana.setVisible(true);
+    }
+    
+}
