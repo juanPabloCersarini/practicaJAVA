dee767797de1fc4dfc9ae2190e1b08b1b0b125d1
juanPabloCesarini <jpablocesarini@gmail.com>
2019-10-16 18:01:07 -0300

    armando un crud super sencillo

--- UtilizandoJDBC/src/utilizandojdbc/UtilizandoJDBC.java
+++ UtilizandoJDBC/src/utilizandojdbc/UtilizandoJDBC.java
@@ -22,15 +22,19 @@ public class UtilizandoJDBC {
         String url = "jdbc:mysql://localhost:3306/"+base;
         String user = "root";
         String password="";
+        String insertar, modificar, borrar, listar;
         
         
         try{
-            
+            borrar= "delete from clientes where idCli = 7";
+            insertar="INSERT INTO clientes (nCli, aCli,dirCli,telCli) values ('Rodrigo','Marquez','lascano 4040','451-451')";
+            modificar="update clientes set dirCli='Nogoya 2496' where nCli='Pedro'";
+            listar ="select * from clientes where nCli = 'Seba'";
             Connection miConexion = DriverManager.getConnection(url, user, password);
             Statement miStatment = miConexion.createStatement();
-             
-            
-            miStatment.executeUpdate("INSERT INTO clientes (nCli, aCli,dirCli,telCli) values ('Rodrigo','Marquez','lascano 4040','451-451')"); // executeQuery();
+            miStatment.execute(borrar);
+            miStatment.executeUpdate(modificar);
+            miStatment.executeUpdate(insertar); 
             ResultSet miRSet = miStatment.executeQuery("select * from clientes");
             
             System.out.println("Nombre: --- Apellido");
@@ -38,8 +42,12 @@ public class UtilizandoJDBC {
                 System.out.println(miRSet.getString("nCli") + " " + miRSet.getString("aCli") + " " + miRSet.getString("dirCli"));
                 
             }
+            System.out.println("--------------------------");
+            ResultSet Rs =miStatment.executeQuery(listar);
             
-            
+            while(Rs.next()){
+                System.out.println(Rs.getString("ncli"));
+            }
         }catch (SQLException e){
             
             System.out.println("error en conexion!");
